a2e35f823ae72ce7f0999273c4f3472e0dc591f0
ibrohim-yusupjonov <110836470+ibrohim-yusupjonov@users.noreply.github.com>
2022-12-20 21:27:14 +0500

    5gacha

---
+++ script.js
@@ -0,0 +1,89 @@
+// #1
+
+// function total(arr){
+// let son = 0
+// arr.forEach( number => {
+//     son += number;
+//     console.log(son);
+// });
+// }
+// let arr = [1, 2, 3];
+// total(arr);
+
+// #2
+
+
+
+
+// function stringConcat(array) {
+//       let add = array.join('');
+//       console.log(add);
+// }
+// stringConcat([1, 2, 3]);
+
+
+
+// #3
+
+
+// var voters = [
+//     { name: 'Bob', age: 30, voted: true },
+//     { name: 'Jake', age: 32, voted: true },
+//     { name: 'Kate', age: 25, voted: false },
+//     { name: 'Sam', age: 20, voted: false },
+//     { name: 'Phil', age: 21, voted: true },
+//     { name: 'Ed', age: 55, voted: true },
+//     { name: 'Tami', age: 54, voted: true },
+//     { name: 'Mary', age: 31, voted: false },
+//     { name: 'Becky', age: 43, voted: false },
+//     { name: 'Joey', age: 41, voted: true },
+//     { name: 'Jeff', age: 30, voted: true },
+//     { name: 'Zack', age: 19, voted: false }
+// ];
+
+// function voterResults(arr) {
+//     voter = 0
+//     // for (let a = 0; a < arr.length; a++){
+//     //      if (arr[a].voted === true){
+//     //             if (arr[a].age >= 18) {
+//     //                   voter++;
+//     //             }
+//     //      }
+//     // }
+//     arr.forEach(vote => {
+//         if ( vote.voted === true){
+//             voter++
+//         }
+//     });
+//     console.log(`voted: True ${voter}`);
+// }
+
+// voterResults(voters);
+
+
+
+// #4
+
+
+// function shoppingSpree(arr) {
+//     let YouPay = 0;
+//     arr.forEach( money => {YouPay += money.price;});
+//     return YouPay;
+// }
+
+// var wishlist = [
+//     { title: "Tesla Model S", price: 90000 },
+//     { title: "4 carat diamond ring", price: 45000 },
+//     { title: "Fancy hacky Sack", price: 5 },
+//     { title: "Gold fidgit spinner", price: 2000 },
+//     { title: "A second Tesla Model S", price: 90000 }
+// ];
+
+// console.log(shoppingSpree(wishlist)); 
+
+
+// #5
+
+
+
+
